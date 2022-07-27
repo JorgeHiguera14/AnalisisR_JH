fff1fa22b722ad90bad518e33ab909e53cb63c99
Jorge Higuera <108103103+JorgeHiguera14@users.noreply.github.com>
2022-07-27 16:42:28 -0500

    Update Taller 3.R

--- Taller 3.R
+++ Taller 3.R
@@ -1,25 +1,26 @@
 # Integrantes
 ## Jorge Higuera 
 ## Andres Rodriguez 
-## Hugo Fonseca 
-## Danny Buitrago 
 
 
 library(tidyverse)
 base <- read.csv("songs_normalize.csv")
 
 
-##1
+### P.1
+
 g1 <- ggplot(Spotify, aes(x = danceability, y = valence)) +
   geom_point(color = "blue", alpha = 0.3)
 
 
-##2
+### P.2
+
 g2 <- ggplot(Spotify, aes(x = as.factor(mode), y = energy, color = as.factor(mode))) +
   geom_boxplot(show.legend = FALSE)
 
 
-##3
+### P.3
+
 install.packages("patchwork")
 library(patchwork)
 g1+g2
